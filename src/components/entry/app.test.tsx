eebb568e754abf87976274e0feeca675af618b58
hjklqw <yourmotherisawhore@live.com>
2020-04-24 00:24:10 -0400

    - Installed Jest and all necessary setup to make the simplest test work

--- src/components/entry/app.test.tsx
+++ src/components/entry/app.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { shallow } from 'enzyme';
 import App from './app.view';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  it('should render', () => {
+    const wrapper = shallow(<App />);
+  });
 });
