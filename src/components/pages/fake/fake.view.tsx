c52579b438c345e36d7c6bc6992fab3a1adb409a
hjklqw <yourmotherisawhore@live.com>
2020-04-26 15:57:11 -0400

    - Wrapped FakePage in the NormalPageLayout for consistency - Made homepage less bare

--- src/components/pages/fake/fake.view.tsx
+++ src/components/pages/fake/fake.view.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import './fake.scss';
+import NormalPageLayout from '../../layout/normalPage';
 
 const FakePage = () => (
-  <div className="fake-page-message">This page is fake, and so is everything else that's highlighted right now!</div>
+  <NormalPageLayout>
+    <div className="fake-page-message">This page is fake, and so is everything else that's highlighted right now!</div>
+  </NormalPageLayout>
 );
 
 export default FakePage;
